846ae80128dabb0ec49a0a2e5976563c79ab6c02
Donovan Chavez <chavezdon17@gmail.com>
2020-09-26 17:03:04 -0500

    added loop

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+// possible vaules
+
+var vaules =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()+";
+
+var password = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
