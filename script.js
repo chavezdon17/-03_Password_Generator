c9ab993967947a664be24e40e7ff0fe8c4d9b4bf
Donovan Chavez <chavezdon17@gmail.com>
2020-09-27 21:22:04 -0500

    made array

--- script.js
+++ script.js
@@ -8,12 +8,32 @@ function writePassword() {
 
   passwordText.value = password;
 }
-// possible vaules
+// array 
+var password_characters["letters"
+"numbers"
+"characters"
+];
 
+// possible vaules
 var vaules =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()+";
 
-var password = "";
+//var password = "";
+
+var complexity = 12;
+
+// loop for password
+
+function generatePassword() {
+  for (var i = 0; i <= complexity; i++) {
+    password =
+      password +
+      vaules.charAt(Math.floor(Math.random() * Math.floor(vaules, length - 1)));
+  }
+}
+
+// add password to display area
+//document.generatePassword("card-body").vaule = password;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
